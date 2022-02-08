a57165bf3c2d84a86172508e1ba8db7dd4315c7b
Iván González <ivangonzalezmahagamage@gmail.com>
2022-02-08 22:35:11 +0000

    Fix docker

--- backend/scripts/docker.sh
+++ backend/scripts/docker.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Start the first process
 go run main.go &
-
-# Start the second process
 swagger serve -F=swagger swagger/swagger.json --port=9090 --no-open &
-
-# Wait for any process to exit
 wait -n
-
-# Exit with status of process that exited first
 exit $?
