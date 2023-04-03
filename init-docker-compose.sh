8ce6d9e9229eedf9dbc3bf3ce3c1acdb7b1d329e
Iván González <cafeteru.dev@gmail.com>
2023-04-03 13:43:16 +0200

    Set LF style files

--- init-docker-compose.sh
+++ init-docker-compose.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 dos2unix.exe backend/scripts/*
-docker compose up -d
-docker rm localize-me-mongo-seed
+docker compose up
--- mongo-seed/start.sh
+++ mongo-seed/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Esperar a que el contenedor de la base de datos esté disponible
 apt-get update
 apt-get install -y netcat curl dos2unix 
 curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh 
