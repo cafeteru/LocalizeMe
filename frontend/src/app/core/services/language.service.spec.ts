73646eb1f05902ec9129d56f49894458d3a1581d
Iván González <cafeteru.dev@gmail.com>
2023-04-12 00:03:34 +0200

    Update LanguageService tests

--- frontend/src/app/core/services/language.service.spec.ts
+++ frontend/src/app/core/services/language.service.spec.ts
@@ -7,6 +7,7 @@ import { createMockLanguage, Language } from '../../types/language';
 describe('LanguageService', () => {
     let service: LanguageService;
     let mockHttp: HttpTestingController;
+    let language: Language;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -14,6 +15,7 @@ describe('LanguageService', () => {
         });
         service = TestBed.inject(LanguageService);
         mockHttp = TestBed.inject(HttpTestingController);
+        language = createMockLanguage();
     });
 
     afterEach(() => {
@@ -24,72 +26,87 @@ describe('LanguageService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('check create', () => {
-        const language = createMockLanguage();
-        const groupDto: Language = {
-            description: language.description,
-            isoCode: language.isoCode,
-            id: undefined,
-            active: true,
-        };
-        service.create(groupDto).subscribe({
-            error: (err) => fail(err),
+    describe('create', () => {
+        it('check valid', () => {
+            const groupDto: Language = {
+                description: language.description,
+                isoCode: language.isoCode,
+                id: undefined,
+                active: true,
+            };
+            service.create(groupDto).subscribe({
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}`);
+            expect(req.request.method).toBe('POST');
+            req.flush(language);
         });
-        const req = mockHttp.expectOne(`${service.url}`);
-        expect(req.request.method).toBe('POST');
-        req.flush(language);
     });
 
-    it('check correct findAll', () => {
-        const response = [createMockLanguage()];
-        service.findAll().subscribe({
-            next: (value) => expect(value).toEqual(response),
-            error: (err) => fail(err),
+    describe('findAll', () => {
+        it('check correct', () => {
+            const response = [language];
+            service.findAll().subscribe({
+                next: (value) => expect(value).toEqual(response),
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}`);
+            expect(req.request.method).toBe('GET');
+            req.flush(response);
         });
-        const req = mockHttp.expectOne(`${service.url}`);
-        expect(req.request.method).toBe('GET');
-        req.flush(response);
+
+        it('should return an empty array if the request fails', () => {
+            service.findAll().subscribe({
+                next: (value) => expect(value).toEqual([]),
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}`);
+            expect(req.request.method).toBe('GET');
+            req.flush('Invalid request', { status: 400, statusText: 'Bad Request' });
+        });
     });
 
-    it('check valid delete', () => {
-        const language = createMockLanguage();
-        service.delete(language).subscribe({
-            next: (res) => expect(res).toBeTrue(),
-            error: (err) => fail(err),
+    describe('delete', () => {
+        it('check valid', () => {
+            service.delete(language).subscribe({
+                next: (res) => expect(res).toBeTrue(),
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}/${language.id}`);
+            expect(req.request.method).toBe('DELETE');
+            req.flush(true);
         });
-        const req = mockHttp.expectOne(`${service.url}/${language.id}`);
-        expect(req.request.method).toBe('DELETE');
-        req.flush(true);
+
+        it('check invalid', () => {
+            service.delete(language).subscribe({
+                next: (res) => expect(res).toBeFalse(),
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}/${language.id}`);
+            expect(req.request.method).toBe('DELETE');
+            req.flush(true, { status: 400, statusText: 'Bad Request' });
+        });
     });
 
-    it('check invalid delete', () => {
-        const language = createMockLanguage();
-        service.delete(language).subscribe({
-            next: (res) => expect(res).toBeFalse(),
-            error: (err) => fail(err),
+    describe('disable', () => {
+        it('check valid', () => {
+            service.disable(language).subscribe({
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}/${language.id}`);
+            expect(req.request.method).toBe('PATCH');
+            req.flush(language);
         });
-        const req = mockHttp.expectOne(`${service.url}/${language.id}`);
-        expect(req.request.method).toBe('DELETE');
-        req.flush(true, { status: 400, statusText: 'Bad Request' });
     });
 
-    it('check disable', () => {
-        const language = createMockLanguage();
-        service.disable(language).subscribe({
-            error: (err) => fail(err),
+    describe('update', () => {
+        it('check valid', () => {
+            service.update(language).subscribe({
+                error: (err) => fail(err),
+            });
+            const req = mockHttp.expectOne(`${service.url}`);
+            expect(req.request.method).toBe('PUT');
+            req.flush(language);
         });
-        const req = mockHttp.expectOne(`${service.url}/${language.id}`);
-        expect(req.request.method).toBe('PATCH');
-        req.flush(language);
-    });
-
-    it('check update', () => {
-        const language = createMockLanguage();
-        service.update(language).subscribe({
-            error: (err) => fail(err),
-        });
-        const req = mockHttp.expectOne(`${service.url}`);
-        expect(req.request.method).toBe('PUT');
-        req.flush(language);
     });
 });
