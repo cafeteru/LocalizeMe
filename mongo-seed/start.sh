1491cb244fbe8aea567c03f930d2f1a981e97b75
Iván González <cafeteru.dev@gmail.com>
2024-08-22 18:39:33 +0200

    Simplify mongo-seed

--- mongo-seed/start.sh
+++ mongo-seed/start.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y netcat curl dos2unix 
-curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh 
-chmod +x wait-for-it.sh 
-dos2unix mongo-seed/*
-./wait-for-it.sh database:27017 
-./mongo-seed/import.sh
+apt-get install -y netcat curl dos2unix
+
+HOST="database"
+PORT="27017"
+TIMEOUT=15
+
+if nc -z "$HOST" "$PORT"; then
+    echo "Host $HOST:$PORT is available after $TIMEOUT seconds"
+else
+    echo "Host $HOST:$PORT is not available within $TIMEOUT seconds"
+fi
+
+MONGO_HOST=$HOST
+MONGO_AUTH_DB=admin
+MONGO_USER=mongoadmin
+MONGO_PASSWORD=secret
+
+COLLECTIONS=("users" "stages" "languages" "groups" "baseStrings")
+for collection in "${COLLECTIONS[@]}"; do
+    mongoimport --host $MONGO_HOST --authenticationDatabase $MONGO_AUTH_DB --username $MONGO_USER --password $MONGO_PASSWORD --db localizeMe --collection $collection --type json --file /mongo-seed/$collection.json --jsonArray
+done
